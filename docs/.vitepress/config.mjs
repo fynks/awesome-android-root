71c7857af5e3e1590835766732bd471204ebd905
fynks <75840152+fynks@users.noreply.github.com>
2024-12-20 11:50:30 +0500

    fix: update favicon link type and add googlebot meta tag for SEO

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   ignoreDeadLinks: true,
   head: [
     // Favicons
-    ['link', { rel: 'icon', href: '/favicon.ico', sizes: 'any' }],
+    ['link', { rel: 'icon', type:'image/x-icon', href: '/favicon.ico' }],
     ['link', { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }],
     ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' }],
     ['link', { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32' }],
@@ -37,6 +37,7 @@ export default defineConfig({
 
     // SEO Meta Tags
     ['meta', { name: 'keywords', content: 'android root, root apps, magisk modules, xposed modules, android customization, android tools, root utilities, android mods, cool rooted android apps, android root tool, root apk for android, adb debugging toolreddit, root apps for android, android tweaking toolbox without root, android os netlify, termux android root, how to root android in termux, android root app, how to get root access in termux without rooting your android root, awesome android root, android rooten, how to root magisk any samsung devices android 14 2024, app root for android, sdf tools android root, how to use terminal emulator in android device root, android jailbreak rooter, android root software, how to root android termux, lsposed zygisk, android root nobody, app root android, how to install parrot os on android without root youtube, debloating solution, awesome root android, android root jar, android run an app as root, awesome android kotlin apps, how to root andoid in temux, kernelsu, install parrot os on android termux non root youtube, android root file name, andid oot, android root tools, install kali linux on android without root andronix termux youtube, termux root android hilfe, adb macrodroid, android root modify app classification, toop 5 root application on android, root genes android, awesome android, aandroid root tool, best apps for rooted android reddit, root android app apk, youtube netlify lite app, git awesome android rot, start android app with root, andri root, apps to root android, root and other apps android, apps for rooting android from play store, rooted andoid 15, xposed installer 官网, apatch root, install hydra on termux in android without root youtube, qmqm.netlify.app, root, they like the applications to be integrated in the android root, android permissions list, tools to root android device, run termux command from kernel mamager, how to install termux on android no root, root package manager android, android hilfe rooten' }],
+    ['meta', { name: 'googlebot', content: 'index, follow' }],
     ['meta', { name: 'author', content: 'Fynks' }],
     ['meta', { name: 'robots', content: 'index, follow' }],
     ['meta', { name: 'language', content: 'English' }]
