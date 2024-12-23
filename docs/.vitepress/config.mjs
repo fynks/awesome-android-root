e238f3a76c6ad763ac9f6df67ef10a22860285b3
fynks <75840152+fynks@users.noreply.github.com>
2024-12-23 09:40:22 +0500

    removed canonnical link from every web page

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -17,7 +17,6 @@ export default defineConfig({
     ['link', { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32' }],
     ['link', { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16' }],
     ['link', { rel: 'manifest', href: '/site.webmanifest' }],
-    ['link', { rel: 'canonical', href: 'https://awesome-android-root.link' }],
 
     // Preconnects and Prefetches
     ['link', { rel: 'dns-prefetch', href: 'https://awesome.re' }],
