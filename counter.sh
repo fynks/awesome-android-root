6a051936d00d15c64d089b089945dcf3e09ef471
fynks <75840152+fynks@users.noreply.github.com>
2025-01-17 11:08:14 +0500

    detailed stats

--- counter.sh
+++ counter.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-grep -E '^- \*\*\[[^]]+\]\(.*\)\*\*' /home/ali/Github/awesome-android-root/README.md | wc -l
+readme_file="README.md"
+
+# Count total app entries
+total_entries=$(grep -E '^- \*\*\[[^]]+\]\(.*\)\*\*' "$readme_file" | wc -l)
+
+# Count Magisk modules
+magisk_modules=$(grep -E '^- \*\*\[[^]]+\]\(.*\)\*\*.*\[M\]' "$readme_file" | wc -l)
+
+# Count LSPosed modules
+lsposed_modules=$(grep -E '^- \*\*\[[^]]+\]\(.*\)\*\*.*\[LP\]' "$readme_file" | wc -l)
+
+# Calculate Root Apps
+root_apps=$((total_entries - magisk_modules - lsposed_modules))
+
+# Output in text
+echo "Total number of app entries: $total_entries"
+echo "Number of Magisk modules: $magisk_modules"
+echo "Number of LSPosed modules: $lsposed_modules"
+echo "Number of Root Apps: $root_apps"
